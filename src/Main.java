3b8fe66388c65a7211ebf789dbee5b388e201305
is8bella <league@WTS5.attlocal.net>
2016-07-10 15:15:51 -0700

    1 and 2 Player

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Main implements KeyListener {
+public class Main implements KeyListener, ActionListener {
 	JFrame Frame;
 	JFrame StartupFrame;
 	JPanel StartupPanel;
-	GamePanel object;
+	JPanel object;
 	BlockObject entity;
 	JButton Player1;
 	JButton Player2;
@@ -29,33 +31,49 @@ public class Main implements KeyListener {
 		Player2 = new JButton("Two Player");
 		StartupPanel = new JPanel();
 		StartupFrame.add(StartupPanel);
-		StartupPanel.addKeyListener(this);
+		Player1.addActionListener(this);
+		Player2.addActionListener(this);
 		StartupPanel.add(Player1);
 		StartupPanel.add(Player2);
-		Frame = new JFrame("Block Escape");
-		//Frame.setVisible(true);
-		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		object = new GamePanel();
-		Frame.add(object);
-		Frame.addKeyListener(this);
-		Frame.setSize(256, 502);
 	}
+
 	public void actionPerformed(ActionEvent e) {
-		 
+		if (e.getSource() == Player1) {
+			object = new GamePanel();
+			Frame = new JFrame("Block Escape");
+			Frame.setVisible(true);
+			Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Frame.add(object);
+			Frame.addKeyListener(this);
+			Frame.setSize(256, 502);
+			StartupFrame.setVisible(false);
+		}
+		if (e.getSource() == Player2) {
+			object = new GamePanel2();
+			Frame = new JFrame("Block Escape");
+			Frame.setVisible(true);
+			Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Frame.add(object);
+			Frame.addKeyListener(this);
+			Frame.setSize(256, 502);
+			StartupFrame.setVisible(false);
+		}
+		
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
-		object.keyTyped(e);
+		((KeyListener) object).keyTyped(e);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		object.keyPressed(e);
+		((KeyListener) object).keyPressed(e);
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		object.keyReleased(e);
+		((KeyListener) object).keyReleased(e);
 	}
 }
